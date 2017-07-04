b2fc282306c490335486d249b1fa266439ee8508
sangavi-k <sangavivalarmathi@gmail.com>
2017-07-04 17:45:51 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+# sangavi
+import java.util.*;
+public class Main{
+public static void main(String args[])
+{
+int num;
+Scanner sc-new Scanner(System.in);
+System.out.println("Enter any integer number: ");
+num=sc.nextInt();
+if(num>0)
+System.out.println(num+ "IS POSITIVE NUMBER.");
+elseif(num<0)
+System.out.println(num+ "IS NEGATIVE NUMBER.");
+else
+System.out.println("IT's ZERO.");
+}
+}
