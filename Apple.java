366e9ff1813256f6b4fd7b824ab64132816827a4
sangavi-k <sangavivalarmathi@gmail.com>
2017-07-06 14:38:45 +0530

    Create Apple.java

---
+++ Apple.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class Apple
+{
+  public static void main(String args[])
+  {
+    int sum=0;
+    int n=10;
+    for(int i=1;i<=n;i++)
+    {
+      sum=sum+i;
+    }
+    System.out.println("The Sum Of "+n+" Numbers are:" + sum);
+  }
+}
