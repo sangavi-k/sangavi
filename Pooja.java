013e6aa8a3ccf584c72816cca05de46981ae8b68
sangavi-k <sangavivalarmathi@gmail.com>
2017-07-06 14:23:50 +0530

    Create Pooja.java

---
+++ Pooja.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class Pooja{
+public static void main(String args[])
+{
+int a,sum;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the integer");
+a=sc.nextInt
+sum=(a*(a+1))/2;
+System.out.println("sum");
+}
+}
